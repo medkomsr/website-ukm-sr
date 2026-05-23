ac0b2121099924304418706059c1611d4c7b7190
BoRrrr <robialwan8@gmail.com>
2026-05-23 20:54:27 +0700

    fix: fixing build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display, Great_Vibes } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import QueryProvider from "@/providers/query-providers";
@@ -21,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id" className={cn("h-full antialiased", inter.variable, playfair.variable, greatVibes.variable, "font-sans")}>
-      <body className="min-h-full flex flex-col"> <QueryProvider>{children}</QueryProvider></body>
+      <body className="min-h-full flex flex-col">
+        <Suspense>
+          <QueryProvider>{children}</QueryProvider>
+        </Suspense>
+      </body>
     </html>
   );
 }
